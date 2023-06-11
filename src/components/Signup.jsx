c55be4b02929ebbe682eb37e7e616df1296b0e65
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-06-11 22:22:58 +0500

    sign up done

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,38 +1,43 @@
-import { signupimg, signupimg2 } from "@assets";
+import { signup1, signup2 } from "@assets";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
   return (
-    <div className="signupsection">
-      <div className="signupsection__content">
-        <div className="signupsection__content__left">
-          <div className="signupsection__content__left__img">
-            <img src={signupimg} alt="Signupimg" />
-
-            <div className="signupsection__content__left__heading">
-              Sign Up as a Seller
-            </div>
-            <div className="signupsection__content__left__info">
-              Send a provider you like? Subscribe and receive meals ever week...
-            </div>
-            <button className="signupsection__content__left__button">
-              Sign Up
-            </button>
+    <div className="signup__section">
+      <div className="signup__section__entry">
+        <img
+          src={signup1}
+          alt="signup1"
+          className="signup__section__entry__img"
+        />
+        <div className="signup__section__entry__overlay">
+          <div className="signup__section__entry__overlay__heading">
+            Sign Up as a Seller
           </div>
+          <div className="signup__section__entry__overlay__info">
+            Send a provider you like? Subscribe and receive meals ever week...
+          </div>
+          <Link to="/" className="signup__section__entry__overlay__link">
+            Sign Up
+          </Link>
         </div>
-        <div className="signupsection__content__left">
-          <div className="signupsection__content__left__img">
-            <img src={signupimg2} alt="Signupimg" />
-
-            <div className="signupsection__content__left__heading">
-              Sign Up as a Seller
-            </div>
-            <div className="signupsection__content__left__info">
-              Send a provider you like? Subscribe and receive meals ever week...
-            </div>
-            <button className="signupsection__content__left__button">
-              Sign Up
-            </button>
+      </div>
+      <div className="signup__section__entry">
+        <img
+          src={signup2}
+          alt="signup2"
+          className="signup__section__entry__img"
+        />
+        <div className="signup__section__entry__overlay">
+          <div className="signup__section__entry__overlay__heading">
+            Sign Up as a Seller
           </div>
+          <div className="signup__section__entry__overlay__info">
+            Send a provider you like? Subscribe and receive meals ever week...
+          </div>
+          <Link to="/" className="signup__section__entry__overlay__link">
+            Sign Up
+          </Link>
         </div>
       </div>
     </div>
