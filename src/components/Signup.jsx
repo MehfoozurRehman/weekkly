b1bd8a23d8a371edd9b7931f3641b9ca9dc3b1cd
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2023-06-11 21:53:49 +0500

    sign up  base done

---
+++ src/components/Signup.jsx
@@ -0,0 +1,43 @@
+import {
+  signupimg,
+  signupimg2
+} from "@assets";
+
+export default function Signup() {
+  return (
+    <div className="signupsection">
+      <div className="signupsection__content">
+        <div className="signupsection__content__left">
+          <div className="signupsection__content__left__img">
+            <img src={signupimg} alt="Signupimg" />
+
+            <div className="signupsection__content__left__heading">
+              Sign Up as a Seller
+            </div>
+            <div className="signupsection__content__left__info">
+              Send a provider you like? Subscribe and receive meals ever week...
+            </div>
+            <button className="signupsection__content__left__button">
+              Sign Up
+            </button>
+          </div>
+        </div>
+        <div className="signupsection__content__left">
+          <div className="signupsection__content__left__img">
+            <img src={signupimg2} alt="Signupimg" />
+
+            <div className="signupsection__content__left__heading">
+              Sign Up as a Seller
+            </div>
+            <div className="signupsection__content__left__info">
+              Send a provider you like? Subscribe and receive meals ever week...
+            </div>
+            <button className="signupsection__content__left__button">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
